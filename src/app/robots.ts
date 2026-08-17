fe7e96cbbc5f638e3a435621c4743a498e23868b
divyo-argha <arghastm@gmail.com>
2026-08-18 00:32:45 +0600

    chore: prepare vercel setup

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,15 @@ export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: `${siteConfig.url}/sitemap.xml`,
+    rules: [
+      {
+        userAgent: "ChatGPT-User",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        disallow: "/",
+      },
+    ],
   };
 }
